ea6d1644015008b24f4d0db8616ac8f1c8598e8a
陈栋 <chendong@chendongdeMacBook-Pro.local>
2018-09-27 22:44:49 +0800

    upload

---
+++ src/main/java/com/dodi/springbootlearn/Girl.java
@@ -0,0 +1,18 @@
+package com.dodi.springbootlearn;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Girl {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String cupSise;
+
+    private Integer age;
+
+}
